c38ee7e3199c0508fcfde0a03cc895d3563c0a5c
unknown <fmreda3@gmail.com>
2020-05-12 23:26:36 +0000

    Updates

---
+++ precache-manifest.a41dc82942d537989b08203cc91dce66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a974a4a6da6dc409c75be70372c67dea",
+    "url": "/facebook/index.html"
+  },
+  {
+    "revision": "50eb96ab4d9c795b47fc",
+    "url": "/facebook/static/css/main.7171aea9.chunk.css"
+  },
+  {
+    "revision": "8a7ceb6002651f07c61b",
+    "url": "/facebook/static/js/2.ed6d8d4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facebook/static/js/2.ed6d8d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50eb96ab4d9c795b47fc",
+    "url": "/facebook/static/js/main.12c49d32.chunk.js"
+  },
+  {
+    "revision": "416a4bf4323054a6a683",
+    "url": "/facebook/static/js/runtime-main.8d48e03f.js"
+  },
+  {
+    "revision": "191b6f1785c196fc4126243c0ca81230",
+    "url": "/facebook/static/media/avatar.191b6f17.jpg"
+  },
+  {
+    "revision": "fe99aca482e5703a5f9154d404008e6d",
+    "url": "/facebook/static/media/post.fe99aca4.jpg"
+  }
+]);
